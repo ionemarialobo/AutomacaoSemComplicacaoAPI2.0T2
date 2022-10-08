30250cdb82e3f683ea343fecb5e926e5062437d7
ionemarialobo <lobodias.ione@gmail.com>
2022-10-08 12:40:08 -0300

    cucumber runner e steps

--- src/test/java/plataformaFilmes/PlataformaFilmesTest.java
+++ src/test/java/plataformaFilmes/PlataformaFilmesTest.java
@@ -3,6 +3,7 @@ package plataformaFilmes;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import maps.LoginMap;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import utils.RestUtils;
@@ -39,22 +40,19 @@ public class PlataformaFilmesTest {
     @BeforeAll
     public static void validarLoginMap(){
         RestUtils.setBaseURI("http://localhost:8080/");
+        LoginMap.initLogin();
 
-        Map<String, String> map = new HashMap<>();
-        map.put("email", "aluno@email.com");
-        map.put("senha", "123456");
-
-        Response response = RestUtils.post(map, ContentType.JSON, "auth");
+        Response response = RestUtils.post(LoginMap.getLogin(), ContentType.JSON, "auth");
 
         assertEquals(200, response.statusCode());
-        token = response.jsonPath().get("token");
+        LoginMap.token = response.jsonPath().get("token");
         System.out.println(token);
     }
 
     @Test
     public void validarConsultaCategorias(){
         Map<String, String> header = new HashMap<>();
-        header.put("Authorization", "Bearer "+token);
+        header.put("Authorization", "Bearer "+LoginMap.token);
 
         Response response = RestUtils.get(header, "categorias");
         assertEquals(200, response.statusCode());
