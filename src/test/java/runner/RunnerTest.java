c57ead06dea5ab2b51ded920b2411a92f07cda37
ionemarialobo <lobodias.ione@gmail.com>
2022-10-14 20:07:43 -0300

    cucumber esquema tags

--- src/test/java/runner/RunnerTest.java
+++ src/test/java/runner/RunnerTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        tags = "",
+        tags = "@login",
         glue = "steps",
         plugin = {"json:target/reports/CucumberReports.json", "pretty"},
         snippets = CucumberOptions.SnippetType.CAMELCASE
--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -1,19 +1,39 @@
 package steps;
 
 import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
 import io.cucumber.java.pt.Quando;
+import io.restassured.http.ContentType;
+import maps.LoginMap;
+import utils.RestUtils;
+
+import java.util.Map;
 
 public class LoginSteps {
-
+    String url = "http://localhost:8080/";
     @Dado("que tenha um payload valido da API de login")
     public void queTenhaUmPayloadValidoDaAPIDeLogin() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        LoginMap.initLogin();
+        RestUtils.setBaseURI(url);
     }
+
+    @Dado("que tenha um payload da API de login com as seguintes informacoes")
+    public void queTenhaUmPayloadDaAPIDeLoginComAsSeguintesInformacoes(Map<String, Object> map) {
+        LoginMap.initLogin();
+        RestUtils.setBaseURI(url);
+        LoginMap.getLogin().putAll(map);
+    }
+
     @Quando("envio uma requisição do tipo POST de login")
     public void envioUmaRequisiçãoDoTipoPOSTDeLogin() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        RestUtils.post(LoginMap.getLogin(), ContentType.JSON, "auth");
     }
+
+
+    @Entao("armazeno o token que recebo do response de login")
+    public void armazenoOTokenQueReceboDoResponseDeLogin() {
+        LoginMap.token = RestUtils.getResponse().jsonPath().get("token");
+    }
+
 
 }
